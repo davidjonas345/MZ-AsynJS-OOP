a8e57af3ea12676430c2ab4b1d8e99dc46c83a1d
Jónás <davidjonas345@gmail.com>
2021-03-06 11:22:12 +0100

    add http-get

--- http-get/fetch.js
+++ http-get/fetch.js
@@ -1,4 +1,10 @@
-//can be deleted
-console.log("Console log works!");
-
+ fetch("https://jsonplaceholder.typicode.com/users")
+    .then(response => {
+        response.json().then(data => {
+            console.log(data);
+            if (!response.ok) throw Error(response.statusText);
+            else return data
+        })
+    .catch(error => console.log("Could not fetch data!", error));
+ });
 
